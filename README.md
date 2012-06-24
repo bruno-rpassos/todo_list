b124fb56a930789cd7952399a38b245240017b30
Marcelo Kanzaki <marcelojk@gmail.com>
2012-06-24 19:41:00 -0300

    boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 todo_list
 =========
+
+Visualização de uma TODO
+
+```c
+Lista de compras do mês
+
+  1. Cerveja
+  2. Doritos
+
+  **** itens completos ****
+
+  3. Hamburger
+  4. Sabão
+  5. Escova de Dentes
+  6. Creme de Barbear
+
+Escolha um item pelo número ou digite [c] para cadastrar um novo.
+```
+
+Ao escolher um ítem pelo número, verificar o mesmo...
+
+Se o item estiver completo apresentar:
+  Digite [i] para marcá-lo com incompleto
+  ou [d] para deletá-lo.
+
+caso contrário:
+  Digite [c] para marcá-lo como completo
+  ou [d] para deletá-lo.
+
---
+++ lib/todo_list.c
@@ -0,0 +1,40 @@
+void menu_inicial() {
+  /*
+    Escolha uma opção:
+      [L] Listar TODOS
+      [C] Cadastrar TODO
+  */
+}
+
+void listar_todos() {
+  // listar as TODOS em ordem alfabetica por default
+  // no final da lista oferecer opções de reordenação:
+  /*
+    [AC] Ordem alfabetica crescente
+    [AD] Ordem alfabetica decrescente
+    [IC] Ordem de inserção crescente
+    [ID] Ordem de inserção decrescente
+
+    ...ou digite o ID da TODO para selecionála.
+  */
+}
+
+void motrar_todo(todos, todo_id) {
+  // lista uma única todo com seus itens cadastrados.
+  // 
+}
+
+// aceita uma lista de TODOS como parametro e insere uma nova TODO nela
+void cadastrar_todo() {
+  printf("Digite o título da TODO:\n");
+  // implementar cadastro de todo
+  // o procedimento deverá impedir que o usuário cadastre uma
+  // TODO sem título.
+}
+
+int main(int argc, char const *argv[])
+{
+  menu_inicial();
+
+  return 0;
+}
